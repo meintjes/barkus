32a986557f7711c3ac9aa3d8eb2e03a3cb631ea0
Ian Meintjes <meintjes@umich.edu>
2015-05-31 21:29:02 -0400

    add BST to supported sets

--- lib/common/sets/sets.dart
+++ lib/common/sets/sets.dart
@@ -7,7 +7,7 @@ class CardSet {
 
 final List<CardSet> supportedSets = [
   new CardSet("Velicta", "VEL"),
-  //new CardSet("Bloodied Streets", "BST"),
+  new CardSet("Bloodied Streets", "BST"),
   //new CardSet("Velicta's Reckoning", "VLR"),
   new CardSet("Fleets of Ossia", "FOS"),
 ];
