a2739e62e396c7bc1b2dac2d601bc47095ae0879
Ian Meintjes <meintjes@umich.edu>
2015-07-10 15:10:13 -0400

    update with july sets

--- lib/common/sets/sets.dart
+++ lib/common/sets/sets.dart
@@ -9,13 +9,22 @@ final List<CardSet> supportedSets = [
   new CardSet("Velicta", "VEL"),
   new CardSet("Bloodied Streets", "BST"),
   new CardSet("Velicta's Reckoning", "VRE"),
+
+  new CardSet("Oneiros", "ONE"),
+
   new CardSet("Fleets of Ossia", "FOS"),
   new CardSet("Solphos", "SOL"),
   new CardSet("Fool's Gold", "FGO"),
+
+  new CardSet("Covert", "COV"),
+
   new CardSet("Taramir", "TAR"),
   new CardSet("Waning Light", "WLI"),
   new CardSet("Age of Darkness", "AOD"),
+
   new CardSet("Eldangard", "ELD"),
   new CardSet("Stormfront", "SFR"),
   new CardSet("Ragnarok", "RGN"),
+
+  new CardSet("Veltrum", "VLT"),
 ];
